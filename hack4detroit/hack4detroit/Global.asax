b347358ab15fb65bc4391a9e59d50bc1e353b278
William O'Sullivan <williamosullivan@gmail.com>
2015-06-12 20:07:56 -0400

    Initial checkin
    First check in

---
+++ hack4detroit/hack4detroit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hack4detroit.MvcApplication" Language="C#" %>
---
+++ hack4detroit/hack4detroit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hack4detroit.Startup))]
+namespace hack4detroit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
